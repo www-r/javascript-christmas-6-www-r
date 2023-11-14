f14d7081ed56eee2705bf53a1632854c3eed05e0
김영은 <jessicak0828@gmail.com>
2023-11-14 21:30:59 +0900

    feat: View 중 OutView 생성(출력 담당)

--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -1,10 +1,36 @@
-
-const OutputView = {
-    printMenu() {
-        Console.print("<주문 메뉴>");
-        // ...
-    }
-    // ...
-}
+import { Console } from '@woowacourse/mission-utils';
+import { UNIT, DISCOUNT_LIST } from '../constants.js';
+const OutputView = class {
+	constructor(){
+		this.print
+		this.printMenu
+		this.printMoney
+		this.printAmount
+		this.printDiscountList
+	}
+	print(message) {
+		Console.print(message);
+	}
+	printMenu(menuName, menuAmount) {
+		Console.print(`${menuName} ${menuAmount}${UNIT.GAE}`);
+	}
+	printMoney(number) {
+		Console.print(`${number.toLocaleString()}${UNIT.WON}`);
+	}
+	printAmount(number) {
+		Console.print(`${number}${UNIT.GAE}`);
+	}
+	printDiscountList(discountList, date) {
+		const isWeekend = date.getIsWeekend();
+		Console.print(`${DISCOUNT_LIST.D_DAY_EVENT}${discountList[0]}UNIT.WON`);
+		if (isWeekend) {
+			Console.print(`-${DISCOUNT_LIST.WEEKEND_EVENT}${discountList[1]}UNIT.WON`);
+		} else {
+			Console.print(`-${DISCOUNT_LIST.WEEKDAY_EVENT}${discountList[1]}UNIT.WON`);
+		}
+		Console.print(`-${DISCOUNT_LIST.STAR_EVENT}${discountList[2]}UNIT.WON`);
+		Console.print(`-${DISCOUNT_LIST.GIVEAWAY_EVENT}${discountList[3]}UNIT.WON`);
+	}
+};
 
 export default OutputView;
