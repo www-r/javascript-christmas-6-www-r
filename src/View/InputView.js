93592284ef3dad1065ea0834bb4f5b86eb2857e8
김영은 <jessicak0828@gmail.com>
2023-11-15 01:34:29 +0900

    feat: View - 출력, 입력 기능 생성

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -1,9 +1,16 @@
-const InputView = {
-    async readDate() {
-        const input = await Console.readLineAsync("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        // ...
-    }
-    // ...
-}
+import { Console } from '@woowacourse/mission-utils';
+// import OutputView from './OutputView.js';
+
+const InputView = class {
+	// constructor() {
+	// 	this.outputView = new OutputView();
+	// }
+	constructor() {
+		this.readData = this.readData;
+	}
+	async readData(message) {
+		return await Console.readLineAsync(message);
+	}
+};
 
 export default InputView;
--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -1,12 +1,12 @@
 import { Console } from '@woowacourse/mission-utils';
-import { UNIT, DISCOUNT_LIST } from '../constants.js';
+import { UNIT, DISCOUNT_LIST, AMOUNT, DIVIDER_HYPHEN, NOTHING } from '../constants.js';
 const OutputView = class {
-	constructor(){
-		this.print
-		this.printMenu
-		this.printMoney
-		this.printAmount
-		this.printDiscountList
+	constructor() {
+		this.print;
+		this.printMenu;
+		this.printMoney;
+		this.printAmount;
+		this.printDiscountList;
 	}
 	print(message) {
 		Console.print(message);
@@ -21,15 +21,41 @@ const OutputView = class {
 		Console.print(`${number}${UNIT.GAE}`);
 	}
 	printDiscountList(discountList, date) {
-		const isWeekend = date.getIsWeekend();
-		Console.print(`${DISCOUNT_LIST.D_DAY_EVENT}${discountList[0]}UNIT.WON`);
-		if (isWeekend) {
-			Console.print(`-${DISCOUNT_LIST.WEEKEND_EVENT}${discountList[1]}UNIT.WON`);
-		} else {
-			Console.print(`-${DISCOUNT_LIST.WEEKDAY_EVENT}${discountList[1]}UNIT.WON`);
+		if (discountList[0] === 0 && discountList[1] === 0 && discountList[2] === 0) {
+			Console.print(AMOUNT.NONE);
+			return;
 		}
-		Console.print(`-${DISCOUNT_LIST.STAR_EVENT}${discountList[2]}UNIT.WON`);
-		Console.print(`-${DISCOUNT_LIST.GIVEAWAY_EVENT}${discountList[3]}UNIT.WON`);
+		const isWeekend = date.getIsWeekend();
+		Console.print(
+			`${DISCOUNT_LIST.D_DAY_EVENT}${discountList[0] === AMOUNT.ZERO ? NOTHING : DIVIDER_HYPHEN}${discountList[0]}${
+				UNIT.WON
+			}`
+		);
+		// console.log('discountList:', discountList);
+		if (isWeekend) {
+			Console.print(
+				`${DISCOUNT_LIST.WEEKEND_EVENT}${
+					discountList[1] === AMOUNT.ZERO ? NOTHING : DIVIDER_HYPHEN
+				}${discountList[1].toLocaleString()}${UNIT.WON}`
+			);
+		}
+		if (!isWeekend) {
+			Console.print(
+				`${DISCOUNT_LIST.WEEKDAY_EVENT}${
+					discountList[1] === AMOUNT.ZERO ? NOTHING : DIVIDER_HYPHEN
+				}${discountList[1].toLocaleString()}${UNIT.WON}`
+			);
+		}
+		Console.print(
+			`${DISCOUNT_LIST.STAR_EVENT}${
+				discountList[2] === AMOUNT.ZERO ? NOTHING : DIVIDER_HYPHEN
+			}${discountList[2].toLocaleString()}${UNIT.WON}`
+		);
+		Console.print(
+			`${DISCOUNT_LIST.GIVEAWAY_EVENT}${
+				discountList[3] === AMOUNT.ZERO ? NOTHING : DIVIDER_HYPHEN
+			}${discountList[3].toLocaleString()}${UNIT.WON}`
+		);
 	}
 };
 
