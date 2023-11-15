1b38dbf63539f8dc5f970ad34c5a4b36c7e0d9b8
김영은 <jessicak0828@gmail.com>
2023-11-15 19:43:51 +0900

    feat: Map 데이터 만드는 방법 변경

--- src/menus.js
+++ src/menus.js
@@ -1,60 +1,88 @@
-const menusData = new Map();
-menusData.set('양송이수프', {
-	course: '에피타이저',
-	price: 6000,
-});
-menusData.set('타파스', {
-	course: '에피타이저',
-	price: 5500,
-});
-menusData.set('시저샐러드', {
-	course: '에피타이저',
-	price: 8000,
-});
-menusData.set('티본스테이크', {
-	//	name: '티본스테이크',
-	course: '메인',
-	price: 55000,
-});
-menusData.set('바비큐립', {
-	//name: '바비큐립',
-	course: '메인',
-	price: 54000,
-});
-menusData.set('해산물파스타', {
-	//name: '해산물파스타',
-	course: '메인',
-	price: 35000,
-});
-menusData.set('크리스마스파스타', {
-	//name: '크리스마스파스타',
-	course: '메인',
-	price: 25000,
-});
-menusData.set('초코케이크', {
-	//name: '초코케이크',
-	course: '디저트',
-	price: 15000,
-});
-menusData.set('아이스크림', {
-	//	name: '아이스크림',
-	course: '디저트',
-	price: 5000,
-});
-menusData.set('제로콜라', {
-	//name: '제로콜라',
-	course: '음료',
-	price: 3000,
-});
-menusData.set('레드와인', {
-	//name: '레드와인',
-	course: '음료',
-	price: 60000,
-});
-menusData.set('샴페인', {
-	//name: '샴페인',
-	course: '음료',
-	price: 25000,
-});
+const menusData = new Map([
+	[
+		'양송이수프',
+		{
+			course: '에피타이저',
+			price: 6000,
+		},
+	],
+	[
+		'타파스',
+		{
+			course: '에피타이저',
+			price: 5500,
+		},
+	],
+	[
+		'시저샐러드',
+		{
+			course: '에피타이저',
+			price: 8000,
+		},
+	],
+	[
+		'티본스테이크',
+		{
+			course: '메인',
+			price: 55000,
+		},
+	],
+	[
+		'바비큐립',
+		{
+			course: '메인',
+			price: 54000,
+		},
+	],
+	[
+		'해산물파스타',
+		{
+			course: '메인',
+			price: 35000,
+		},
+	],
+	[
+		'크리스마스파스타',
+		{
+			course: '메인',
+			price: 25000,
+		},
+	],
+	[
+		'초코케이크',
+		{
+			course: '디저트',
+			price: 15000,
+		},
+	],
+	[
+		'아이스크림',
+		{
+			course: '디저트',
+			price: 5000,
+		},
+	],
+	[
+		'제로콜라',
+		{
+			course: '음료',
+			price: 3000,
+		},
+	],
+	[
+		'레드와인',
+		{
+			course: '음료',
+			price: 60000,
+		},
+	],
+	[
+		'샴페인',
+		{
+			course: '음료',
+			price: 25000,
+		},
+	],
+]);
 
 export default menusData;
